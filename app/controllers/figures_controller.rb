0868b17bff58b09bb1b1916c093345a711c5fc5c
KerryAlsace <kerrydougherty@gmail.com>
2016-08-31 12:56:02 -0400

    set up basic views

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,2 +1,35 @@
 class FiguresController < ApplicationController
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
+
+  post '/posts' do
+    @figure = Figure.create(params["figure"])
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/posts/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.name = params[:name]
+    #probs needa assign landmarks here
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,35 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/posts' do
+    @landmark = Landmark.create(params["landmark"])
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by_id(params[:id])
+    erb :'/landmarks/edit'
+  end
+
+  patch '/posts/:id' do
+    @landmark = Landmark.find_by_id(params[:id])
+    @landmark.name = params[:name]
+    #probs needa assign figure here
+    @landmark.save
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
 end
